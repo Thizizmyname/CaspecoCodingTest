b1db0b7a723c84ccf83b864aff02629bdbd4aedf
Thizizmyname <Alexis.remmers@gmail.com>
2020-10-23 17:09:07 +0200

    Explanation as comments

--- three_explain.js
+++ three_explain.js
@@ -5,12 +5,41 @@
  //**************************************************************************************************** 
 
 
+ /*
+  * Note: Majority of this evaluation is dependent on 6 seemingly asynchronous elements are loaded,
+  * otherwise there are multiple loading-bars rendered which will most likely be updated once the data is loaded.
+  * 
+  * The entire function revolves around groups of bookings.
+  * A group is extracted from a selected unit.
+  * I did not quite understand what these groups, or units could represent (Restaurants?, Companies?, Set of tables?),
+  * But the groups have a relation to the individual articles, so one theory could be that a group is
+  * a filter of all bookings that include a specific article-type.
+  * 
+  * Each group gets a list of bookings, and each booking acquires a list of articles.
+  * The function returns a column layout of two columns, which will be inserted and rendered by the caller.
+  * 
+  * Column 1:  Unit Controller
+  * A table of "units", when selecting a unit the "handleSelectUnit" will be called,
+  * this will probably re-run this function based on a new selected unit.
+  * 
+  * Column 2:  Group-render.
+  * A List of groups, which each contains a list of bookings, with their individual list of articles.
+  * The content of these object is what the functions main evaluation is about
+  * (Seem to not be dictated how these are ordered in this function other then they are contained in a VerticalLayout, 
+  * suggesting they will produce a vertical scrollwheel downwards if overflown from the window-size)
+  * 
+  * This Group-render is controlled by a "dateControl" which handle a time period, suggesting you can filter the rendering based on time period.
+  * 
+  * Below are comments or explanations of the evaluation.
+  */
+
  function explainThisFunc() {
 	var self = this
 	var groupsToRender = []
 	var hasEditPermissions = this.hasPermission('booking.update')
 	var actions = []
 
+    // Checking for edit permission and adds a "new booking button"
 	if (hasEditPermissions) {
 		actions.push(
 			<Button
@@ -22,6 +51,7 @@
 		)
 	}
 
+    // Checking if asynchronous call is finished and accessible
 	var allLoaded =
 		isLoaded(this.state.bookingsResult) &&
 		isLoaded(this.state.bookedArticlesResult) &&
@@ -35,6 +65,7 @@
 
 		var groups = this.state.selectedUnit.articleGroups
 
+        // Filter invalid Groups
 		var filteredGroups = groups.filter(
 			(articleGroup) =>
 				tables.findIndex((t) => t.articleGroupId === articleGroup.id) > -1,
@@ -45,12 +76,17 @@
 			name: this.t('booking.hasNoTables'),
 		})
 
+        // Add an unspecified group
 		filteredGroups = filteredGroups.push(unspecifiedGroup)
 
+
+        // For each group of filteredGroup
 		filteredGroups.forEach(function (group) {
 			var bookings = Immutable.List()
 
+            // For each booking from state.bookingsResult
 			self.state.bookingsResult.value.forEach(function (booking) {
+                // Add all articles associated with the current booking
 				booking = booking.set(
 					'articles',
 					BookingStore.getBookedArticlesForBookingResult(
@@ -60,18 +96,28 @@
 					).value,
 				)
 
+                // Constructs a filtered list of the current bookings' articles,
+                // filter based on the current group
 				var articlesInGroup = booking.articles.filter(
 					(a) => a.articleGroupId === group.id,
 				)
 
+                // If there are any articles in the current booking, associated by the current group
+				// (or if there are no articles and its the unspecified group)
+				//
+                // Add this booking (with all its articles) to the list of bookings
+                // that will be related to the current group
 				if (
 					articlesInGroup.count() > 0 ||
 					(booking.articles.count() < 1 && group === unspecifiedGroup)
 				) {
 					bookings = bookings.push(booking)
 				}
-			})
+            }) 
+            // End of foreach booking in group.
+            // At this point, "bookings" contain all bookings related to the current group.
 
+            // Sort the bookings based on start-time
             bookings = bookings.sort(function (a, b) {
 				var moment1 = Moment(a.start, 'YYYY-MM-DDTHH:mm')
 
@@ -81,6 +127,7 @@
 			})
 
 
+            // save the current group as a <BookingGroup> with all its bookings as a list of <BookingListItem>
 			var bookingsToRender = []
 			bookings.forEach(function (booking) {
 				bookingsToRender.push(
@@ -106,13 +153,15 @@
 					{bookingsToRender}
 				</BookingGroup>,
 			)
-		})
+        })
+        //End of foreach group
 	}
 
 
 	var hasValidDaynote = this.hasValidDaynote()
 	var daynote = this.getDaynoteText()
 
+    //dateController
 	var dateControl = (
 		<div className="bookingView_header">
 			<ToolBar wrapping="nowrap" className="bookingView_header_toolBar">
@@ -133,7 +182,8 @@
 						dataValueField="name"
 						dataTextField="text"
 						value={this.state.selectedTimePeriodStatistics}
-						fieldName="timePeriod"
+                        fieldName="timePeriod"
+                        // handleTimePeriodChange, render-filter?
 						onChange={this.handleTimePeriodChange}
 					/>
 				    : null}
@@ -160,6 +210,7 @@
 				onActiveChanged={this.handleActiveColumnChange}
 			>
 				<Column
+                // Unit Controller
 					sidebar={true}
 					canGoLeft={true}
 					name="PickUnit"
@@ -170,7 +221,9 @@
 							<ui.TableView
 								id={'units'}
 								items={this.state.unitsResult.value}
-								itemKey={'id'}
+                                itemKey={'id'}
+                                // handleSelectUnit. Probably dictates the group in the beginning of function and reruns this function upon selection,
+								// (referencing: var groups = this.state.selectedUnit.articleGroups)
 								onSelectItem={this.handleSelectUnit}
 								selectedItem={null}
 								searchBar={false}
@@ -190,6 +243,7 @@
 				</Column>
 
 				<Column
+                // Group-render
 					sidebar={true}
 					canGoLeft={true}
 					name="PickBooking"
@@ -199,13 +253,15 @@
 				>
 					
 					<VerticalLayout>
-                        {allLoaded ? groupsToRender
+                        { // Where the evaluated groups are inserted
+                        allLoaded ? groupsToRender
                             : <ui.FlashBox type="loading" />}
 					</VerticalLayout>
 				</Column>
 			</ColumnLayout>
 
-			{this.state.newBooking && hasEditPermissions ?
+            { // Only displayed if in New-booking mode?
+            this.state.newBooking && hasEditPermissions ?
 				<EditBooking
 					createMode       = {true}
 					unit             = {this.state.selectedUnit}
@@ -214,7 +270,8 @@
 				/>
 			: false}
 
-			{this.state.editBooking ?
+            { // Only displayed if in Edit-booking mode?
+            this.state.editBooking ?
 				<EditBooking
 					createMode={false}
 					unit={this.state.selectedUnit}
